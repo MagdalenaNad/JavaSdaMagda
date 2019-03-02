31842b18e4864276b5078dd77a7ed81f8111b119
MagdalenaNad <m_nadratowska@o2.pl>
2019-03-02 09:23:37 +0100

    Próba synchronizacji

---
+++ src/FahrenheitConverterTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FahrenheitConverterTest {
+    public static void main(String[] args) {
+        FahrenheitConverter fahrenheitConverter = new FahrenheitConverter();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jeżeli chcesz zamienić temperaturę ze skali Fahrenheita na temperaturę w skali Celsjusza wciśnij '1',");
+        System.out.print("jeśli odwrotnie, wciśnij '2': ");
+        int skala = scanner.nextInt();
+
+        if (skala == 1) {
+            System.out.print("Podaj temperatuję w skali Fahrenheita: ");
+            double fahr = scanner.nextDouble();
+            fahrenheitConverter.convertToCelsius(fahr);
+            System.out.print("W skali Celsjusza: ");
+        } else if (skala == 2) {
+            System.out.print("Podaj temperatuję w skali Celsjusza: ");
+            double cels = scanner.nextDouble();
+            fahrenheitConverter.convertToFahrenheit(cels);
+            System.out.print("W skali Fahrenheita: ");
+        } else {
+            System.out.println("Nie podałeś liczby poprawnie.");
+        }
+    }
+}
